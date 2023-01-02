5f24a1ca163965343be345c96e012e8d2a3fac13
BenjaminHalko <benjaminhalko@hotmail.com>
2023-01-01 17:19:59 -0800

    Update

--- root/app.js
+++ root/app.js
@@ -7,12 +7,10 @@ const numCircles = 5;
 const minHue = 190;
 const maxHue = 285;
 
-const spd = 0.05 / 30;
+const spd = 0.05;
 
 const startRadius = 200;
 const endRadius = 500;
-
-// ** DO NOT EDIT BELOW THIS LINE **
 
 // Setup the canvas
 document.body.style.background = "black";
@@ -20,6 +18,7 @@ document.body.innerHTML = `<canvas id='canvas' style='position:fixed;left:0;top:
 ` + document.body.innerHTML;
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+var lastTime = 0;
 
 // Create the circles
 var circles = [];
@@ -34,16 +33,27 @@ for (var i = 0; i < numCircles; i++) {
     dir = (dir + 360 / numCircles + 50 * Math.random()) % 360;
 }
 
-setInterval(function() {
+function animate(timeStamp) {
+    // Resize the canvas
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    // Calculate the elapsed time since the last frame
+    let elapsedTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+
+    // If the elapsed time is too large, reset it
+    if (elapsedTime > 500) elapsedTime = 1;
+
+    // Clear the canvas
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
     
-    for(var i = 0; i < circles.length; i++) {
-        circles[i].r += spd;
+    for(let i = 0; i < circles.length; i++) {
+        // Scale the animation value based on elapsed time
+        circles[i].r += spd * (elapsedTime / 1000);
 
         if (circles[i].r >= 1) {
-            console.log(i);
-            circles[i].r = 0;
+            circles[i].r = circles[i].r % 1;
             circles[i].x = Math.round(Math.random()*Math.cos(dir * Math.PI / 180) * window.innerWidth / 2 + window.innerWidth / 2);
             circles[i].y = Math.round(Math.random()*Math.sin(dir * Math.PI / 180) * window.innerHeight / 2 + window.innerHeight / 2);
             circles[i].color = minHue + Math.round(Math.random() * (maxHue - minHue));
@@ -55,8 +65,14 @@ setInterval(function() {
         ctx.fillStyle.addColorStop(1, 'hsla(' + circles[i].color + ', 100%, 15%, 0)');
         ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
-}, 1000 / 30);
+
+    requestAnimationFrame(animate);
+};
+
+requestAnimationFrame(animate);
 })();
+
+// Buttons
 
 const handleMouseMove = e => {
     const { currentTarget: target } = e;
