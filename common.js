2c37f9842893094c67d2f723621ee268915b37d2
Benjamin <benjaminhalko@hotmail.com>
2024-05-28 08:01:00 -0700

    Updated Game Pages

--- common.js
+++ common.js
@@ -8,7 +8,7 @@ function getPages() {
     };
 
     // Google Play
-    const googlePlayOptions = {
+    const gameOptions = {
         'makenewfriends': {
             name: 'Make New Friends',
             itchio: 'make-new-friends',
@@ -46,15 +46,26 @@ function getPages() {
         }
     };
 
-    for(const name in googlePlayOptions) {
-        googlePlayOptions[name].id = name;
+    for(const name in gameOptions) {
+        gameOptions[name].id = name;
+        // Legacy
         pages[`google-play/${name}`] = {
-            template: 'googleplay/home',
-            options: googlePlayOptions[name]
+            template: 'games/home',
+            options: gameOptions[name]
         };
         pages[`google-play/${name}/privacypolicy.html`] = {
-            template: 'googleplay/privacypolicy',
-            options: googlePlayOptions[name]
+            template: 'games/privacypolicy',
+            options: gameOptions[name]
+        };
+
+        // New
+        pages[`${name}`] = {
+            template: 'games/home',
+            options: gameOptions[name]
+        };
+        pages[`${name}/privacypolicy`] = {
+            template: 'games/privacypolicy',
+            options: gameOptions[name]
         };
     }
 
